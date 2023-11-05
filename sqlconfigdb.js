0c498e6faab887d24ea5da208c45025cb45be4b8
Antara Banerjee <76731553+antarabanerjee@users.noreply.github.com>
2023-11-05 22:54:56 +0530

    Add files via upload

---
+++ sqlconfigdb.js
@@ -0,0 +1,14 @@
+const config = {
+    user:'sa',
+    password:'Antara@1101',
+    server: 'localhost',       
+    database:'Celebal',
+    options:{
+        trustedConnection:true,
+        enableArithAbort: true,
+        encrypt: false,
+        instancename:'MSSQLTEST'
+    },
+    port:1433
+}
+module.exports = config;
